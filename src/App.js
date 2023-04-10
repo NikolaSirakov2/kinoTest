f646dd683bebc0e120e54ff05356c319a5213d03
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-04-10 23:56:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 
   return (
-      <HomePage/>
+      <Routes>
+        <Route index element={<Navigate to={'/home'}/>}></Route>
+        <Route path={'/home'} element={<HomePage/>}></Route>
+      </Routes>
   );
 }
 
