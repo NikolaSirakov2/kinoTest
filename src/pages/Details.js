17724cd48b2bc08ee8df0ac13d6c54d7f71644d7
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-04-13 22:08:05 +0300

    Create Details.js

---
+++ src/pages/Details.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Details.css"
+
+export default class DetailsPage extends React.Component {
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return <>
+        <h1>Details</h1>
+        </>
+    }
+
+}
