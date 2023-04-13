52143f137259cf3c5006f72e916e6045aa8f6e4e
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-04-13 22:07:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home';
+import DetailsPage from './pages/Details';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Navigate to={'/home'}/>}></Route>
         <Route path={'/home'} element={<HomePage/>}></Route>
+        <Route path={'/details'} element={<DetailsPage/>}></Route>
       </Routes>
   );
 }
